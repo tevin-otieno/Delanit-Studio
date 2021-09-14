81d2ed917ee020280927a19dd204419aff420112
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-09-14 11:14:03 +0300

    making the toggle jquery elements to make it more reactive

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 $( ".toggleDes" ).click(function() {     
-    if($('#another-element:visible').length)
-        $('#another-element').hide();
-    else
-        $('#another-element').show();        
-});
+    if($('.txtDes:visible')){
+        $('.imgDes').hide();
+        $('.txtDes').show();
+    }else{
+        $('.imgDes').show();
+        $('.txtDes').hide();
+
+}});
 $( ".toggleDev" ).click(function() {     
-    if($('#another-element:visible').length)
-        $('#another-element').hide();
-    else
-        $('#another-element').show();        
-});
+    if($('.txtDev:visible')){
+        $('.imgDev').hide();
+        $('.txtDev').show();
+    }else{
+        $('.imgDev').show();
+        $('.txtDev').hide();       
+}});
 $( ".togglePM" ).click(function() {     
-    if($('#another-element:visible').length)
-        $('#another-element').hide();
-    else
-        $('#another-element').show();        
-});
+    if($('.txtPM:visible')){
+        $('.imgPM').hide();
+        $('.txtPM').show();
+    }else{
+        $('.imgPM').show();
+        $('.txtPM').hide();        
+}});
