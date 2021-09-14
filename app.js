f07f7fb67b532db7f2cee65d58c38f6110ba6a30
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-09-14 09:57:54 +0300

    making the toggle jquery bit

---
+++ app.js
@@ -0,0 +1,18 @@
+$( ".toggleDes" ).click(function() {     
+    if($('#another-element:visible').length)
+        $('#another-element').hide();
+    else
+        $('#another-element').show();        
+});
+$( ".toggleDev" ).click(function() {     
+    if($('#another-element:visible').length)
+        $('#another-element').hide();
+    else
+        $('#another-element').show();        
+});
+$( ".togglePM" ).click(function() {     
+    if($('#another-element:visible').length)
+        $('#another-element').hide();
+    else
+        $('#another-element').show();        
+});
