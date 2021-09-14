7147d3e5c6878d006c07fb82986ed3dee271be4b
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-09-14 12:14:18 +0300

    making the toggle jquery elements to toggle back and be reactive

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-$( ".toggleDes" ).click(function() {     
-    if($('.txtDes:visible')){
-        $('.imgDes').hide();
-        $('.txtDes').show();
-    }else{
-        $('.imgDes').show();
-        $('.txtDes').hide();
-
-}});
-$( ".toggleDev" ).click(function() {     
-    if($('.txtDev:visible')){
-        $('.imgDev').hide();
-        $('.txtDev').show();
-    }else{
-        $('.imgDev').show();
-        $('.txtDev').hide();       
-}});
-$( ".togglePM" ).click(function() {     
-    if($('.txtPM:visible')){
-        $('.imgPM').hide();
-        $('.txtPM').show();
-    }else{
-        $('.imgPM').show();
-        $('.txtPM').hide();        
-}});
+$(document).ready(function(){
+    $( "#toggleDes" ).click(function() {     
+        if($('#txtDes:visible')){
+            $('#imgDes').hide();
+            $('#txtDes').show();
+        }else{
+            $('#imgDes').show();
+            $('#txtDes').hide();
+    
+    }});
+    $( "#toggleDev" ).click(function() {     
+        if($('#txtDev:visible')){
+            $('#imgDev').hide();
+            $('#txtDev').show();
+        }else{
+            $('#imgDev').show();
+            $('#txtDev').hide();       
+    }});
+    $( "#togglePM" ).click(function() {     
+        if($('#txtPM:visible')){
+            $('#imgPM').hide();
+            $('#txtPM').show();
+        }else{
+            $('#imgPM').show();
+            $('#txtPM').hide();        
+    }});
+})
